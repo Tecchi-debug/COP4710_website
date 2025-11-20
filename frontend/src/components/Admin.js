586270fcbd1678e8dc517342e00317e023f31dec
Nelson Oliva <nelson.ricardo02@gmail.com>
2025-11-20 13:51:31 -0500

    added sections for member types

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { setTypeHelper } from '../helper/Admin_helper';
 /*
 TODO: 
 Four report types:
@@ -22,17 +23,42 @@ function Admin() {
             <p>Welcome to the admin panel.</p>
             <section>
                 <h2>Generate Reports</h2>
-                <div className="report-type">
+                <section className="report-type">
                     <label htmlFor="report-type">Report Type:</label>
-                    <select id="report-type" value={reportType} onChange={(e) => setReportType(e.target.value)}>
+                    <select id="report-type" value={reportType} onChange={(e) => setTypeHelper(e, setReportType)}>
                         <option value="">Select a report type</option>
                         <option value="annual-restaurant">Restaurant Annual Report</option>
                         <option value="annual-customer">Customer Annual Report</option>
                         <option value="annual-donor">Year-end Donation Report</option>
                         <option value="annual-needy">Annual Free Plate Report</option>
                     </select>
-                </div>
-                <div>{reportType}</div>
+                </section>
+                <section className="report-form">
+                    <h2>Section will be responsible to display the form for the selected report type</h2>
+                    <p>Selected report type: {reportType}</p>
+                </section>
+                <section className="report-display">
+                    <p>Section will be responsible to display the report for the selected report type after the form is submitted</p>
+                </section>
+            </section>
+            <section>
+                <h2>Member Reports</h2>
+                <section className="member-type">
+                    <label htmlFor="member-type">Member Type:</label>
+                    <select id="member-type" value={memberType} onChange={(e) => setTypeHelper(e, setMemberType)}>
+                        <option value="">Select a member type</option>
+                        <option value="find-all">Get All Members</option>
+                        <option value="find-member-type">Find by Member Type</option>
+                        <option value="find-email">Find by Email</option>
+                    </select>
+                </section>
+                <section className="member-form">
+                    <h2>Section will be responsible to display the form for the selected member type</h2>
+                    <p>Selected member type: {memberType}</p>
+                </section>
+                <section className="member-display">
+                    <p>Section will be responsible to display the member information for the selected member type after the form is submitted</p>
+                </section>
             </section>
         </div>
     );
--- frontend/src/helper/Admin_helper.js
+++ frontend/src/helper/Admin_helper.js
@@ -1,1 +1,10 @@
 // File for any helper functions/ components needed for admin panel
+export function setTypeHelper(event, setFunction) {
+    const type = event.target.value;
+    
+    if (type === "") {
+        return;
+    } else {
+        setFunction(type);
+    }
+}
