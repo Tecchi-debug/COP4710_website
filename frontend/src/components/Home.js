2574c888ccc6061a5d358bc71b137f453d2a8476
Nelson Oliva <nelson.ricardo02@gmail.com>
2025-12-03 15:52:23 -0500

    fixed title

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div className="site-container">
-      <h1>Welcome to Waste No Kitchen!</h1>
+      <h1>Welcome to Waste Not Kitchen!</h1>
     </div>
   );
 }
